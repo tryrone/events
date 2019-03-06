9f7fb9655e79bdb4847cae35aa7d8232ca127d18
tryrone <tegararuvwe@gmail.com>
2019-03-06 18:50:12 +0100

    commit

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, {
+      useNewUrlParser: true
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
