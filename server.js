887e0fe1cae0438e27c3c8a01df7a0d17f711b79
tryrone <tegararuvwe@gmail.com>
2019-03-06 18:39:19 +0100

    commit

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
-//Connect to mongoDB
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
-mongoose.connect('mongodb://localhost/event',
-    {
-        useNewUrlParser: true
-    }
-);
+const app = express();
 
-mongoose.connection.once('open', function(){
-    console.log('mongoDB connected .... create....');
-}).on('error', function(error){
-    console.log('connection failed....', error)
-});
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
