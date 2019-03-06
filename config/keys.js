c6a96b81924954a8313c77721d8e0bd9886d4a07
tryrone <tegararuvwe@gmail.com>
2019-03-06 19:10:17 +0100

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost/event'
+     mongoURI: 'mongodb://localhost/event'
+    // mongoURI: 'mongodb://tega:tega123@ds161345.mlab.com:61345/events'
   };
