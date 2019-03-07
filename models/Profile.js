403837df3fcda46a8ab70c627e6519b7cfcfd4f9
tryrone <tegararuvwe@gmail.com>
2019-03-07 14:12:49 +0100

    models

---
+++ models/Profile.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create Schema
+
+const ProfileSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      website: {
+        type: String
+      },
+      location: {
+        type: String
+      },
+      bio: {
+        type: String
+      },
+       from: {
+            type: Date,
+            required: true
+          },
+          to: {
+            type: Date
+          },
+          current: {
+            type: Boolean,
+            default: false
+          },
+          description: {
+            type: String
+          },
+      social: {
+        youtube: {
+          type: String
+        },
+        twitter: {
+          type: String
+        },
+        facebook: {
+          type: String
+        },
+        instagram: {
+          type: String
+        }
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
+
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create Schema
+
+const UserSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        data: Buffer,
+        contentType: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
+
