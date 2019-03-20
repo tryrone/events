efd7e9fecf87e08bab75602f00f5edd4076f4640
tryrone <tegararuvwe@gmail.com>
2019-03-20 21:08:47 +0100

    commit

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // DB Config
 const db = require('./config/keys').mongoURI;
+
+//body-parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
